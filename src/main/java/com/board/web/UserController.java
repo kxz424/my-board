cac594e69a03abcd24057b198e51cfb4e6b12a5b
DESKTOP-OIU1QEN\win10 <kxz424@naver.com>
2017-04-05 20:17:54 +0900

    H2 db연결

--- src/main/java/com/board/web/UserController.java
+++ src/main/java/com/board/web/UserController.java
@@ -3,28 +3,33 @@ package com.board.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.board.domain.User;
+import com.board.domain.UserRepository;
+
 @Controller
 public class UserController {
 	
-	private List<User> users = new ArrayList<>();
+	@Autowired
+	private UserRepository userRepository;
 	
-	@PostMapping("/create")
+	@PostMapping("/user/create")
 	public String create(User user) {
 		
 		System.out.println("user : " + user);
-		users.add(user);
-		return "redirect:/list";
+		userRepository.save(user);
+		return "redirect:/user/list";
 	}
 	
-	@GetMapping("/list")
+	@GetMapping("/user/list")
 	public String list(Model model) {
-		model.addAttribute("users", users);
-		return "list";
+		model.addAttribute("users", userRepository.findAll());
+		return "/user/list";
 	}
 
 }
