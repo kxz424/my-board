3d7e5f110afc479a1502398ac795a9dd14946ba3
DESKTOP-OIU1QEN\win10 <kxz424@naver.com>
2017-04-06 15:41:41 +0900

    회원가입, 수정, 사용자 목록

--- src/main/java/com/board/web/UserController.java
+++ src/main/java/com/board/web/UserController.java
@@ -1,35 +1,71 @@
 package com.board.web;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.board.domain.User;
 import com.board.domain.UserRepository;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
+	
 	
 	@Autowired
 	private UserRepository userRepository;
 	
-	@PostMapping("/user/create")
+	@GetMapping("/form")
+	public String form() {
+		return "/user/form";
+	}
+	
+	@PostMapping("")
 	public String create(User user) {
 		
 		System.out.println("user : " + user);
 		userRepository.save(user);
-		return "redirect:/user/list";
+		return "redirect:/users";
 	}
 	
-	@GetMapping("/user/list")
+	@GetMapping("")
 	public String list(Model model) {
 		model.addAttribute("users", userRepository.findAll());
 		return "/user/list";
 	}
+	
+	@GetMapping("{id}/form")
+	public String updateForm(@PathVariable Long id, Model model) {
+		User user = userRepository.findOne(id);
+		model.addAttribute("user", user);
+		return "/user/updateForm";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "/user/login";
+	}
+	
+//	@PostMapping("/{id}")
+//	public String update(@PathVariable Long id, User newUser) {
+//		User user = userRepository.findOne(id);
+//		user.update(newUser);
+//		userRepository.save(user);
+//		return "redirect:/users";
+//	}
+	
+	@PutMapping("/{id}")
+	public String update(@PathVariable Long id, User newUser) {
+		User user = userRepository.findOne(id);
+		user.update(newUser);
+		userRepository.save(user);
+		return "redirect:/users";
+	}
+	
 
 }
