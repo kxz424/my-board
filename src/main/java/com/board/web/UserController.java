0095ea190b26d6c364048bafe6e96f89bfa98a27
DESKTOP-OIU1QEN\win10 <kxz424@naver.com>
2017-04-07 17:13:28 +0900

    자기자신에 한해 개인정보 수정 가능하도록 구현

--- src/main/java/com/board/web/UserController.java
+++ src/main/java/com/board/web/UserController.java
@@ -1,6 +1,7 @@
 package com.board.web;
 
 import javax.servlet.http.HttpSession;
+import javax.websocket.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -41,8 +42,14 @@ public class UserController {
 		}
 		
 		System.out.println("Login Success!");
-		session.setAttribute("user", user);
+		session.setAttribute("sessionedUser", user);
 		
+		return "redirect:/";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("sessionedUser");
 		return "redirect:/";
 	}
 	
@@ -66,7 +73,17 @@ public class UserController {
 	}
 	
 	@GetMapping("{id}/form")
-	public String updateForm(@PathVariable Long id, Model model) {
+	public String updateForm(@PathVariable Long id, Model model, HttpSession session) {
+		Object tempUser = session.getAttribute("sessionedUser");
+		if(tempUser == null) {
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = (User)tempUser;
+		if(!id.equals(sessionedUser.getId())) {
+			throw new IllegalStateException("자신의 정보만 수정할 수 있습니다.");
+		}
+		
 		User user = userRepository.findOne(id);
 		model.addAttribute("user", user);
 		return "/user/updateForm";
@@ -81,9 +98,20 @@ public class UserController {
 //	}
 	
 	@PutMapping("/{id}")
-	public String update(@PathVariable Long id, User newUser) {
+	public String update(@PathVariable Long id, User updatedUser, HttpSession session) {
+		
+		Object tempUser = session.getAttribute("sessionedUser");
+		if(tempUser == null) {
+			return "redirect:/users/loginForm";
+		}
+		
+		User sessionedUser = (User)tempUser;
+		if(!id.equals(sessionedUser.getId())) {
+			throw new IllegalStateException("자신의 정보만 수정할 수 있습니다.");
+		}		
+		
 		User user = userRepository.findOne(id);
-		user.update(newUser);
+		user.update(updatedUser);
 		userRepository.save(user);
 		return "redirect:/users";
 	}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('jongsang', 'jong1234!', '김종상', 'kjs424@nobiz.co.kr');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('ijt', 'ijt', '임지택', 'ijt@nobiz.co.kr');
+INSERT INTO USER (USER_ID, PASSWORD, NAME, EMAIL) VALUES ('jjm', 'jjm', '정진만', 'jjm@nobiz.co.kr');
